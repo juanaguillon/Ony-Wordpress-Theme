1c94f2eb8d73174d9187db331623fb51f2eed519
Juan Aguillon <miloca.98@hotmail.com>
2018-10-11 14:42:59 -0500

    Solucionando Indentación

--- page.php
+++ page.php
@@ -12,46 +12,40 @@
  			 the_post();
  			
  			?>
- 			<div class='post-<?php the_ID(); ?>' <?php post_class( 'post single'); ?>>
+ 			<div class='post-<?php the_ID(); ?>' <?php post_class( 'post single'); ?> >
  				<?php if ( has_post_thumbnail() ) : ?>	
-				<div class="thumdail_post">
-				<?php the_post_thumbnail( 'medium'); ?>
-				</div>
- 			</div> <!-- /thumdail_post -->			
- 			<?php
- 							endif;
+					<div class="thumdail_post">
+					<?php the_post_thumbnail( 'medium'); ?>
+					</div> 				
+ 				<?php endif; ?>
 
- 			
- 		?>
+		 		<div class="post_header_title">
+		 			<h2 class="post_title"><?php the_title(); ?></h2>
+		 		</div>
 
- 		<div class="post_header_title">
- 			<h2 class="post_title"><?php the_title(); ?></h2>
- 		</div>
+		 		<div class="post_content">
+		 			<?php the_content(); ?>		 				
+		 		</div>
+				
+				<?php 
+					$args = array(
+						'before'        => '<div class="clearing"></div><p class="page_links"><span class="links_title"> ' . __( 'Pages:' , 'ony') .'</span>',
+						'after'         => '</p></div>',
+						'link_before'   => '<span>',
+						'link_after'    => '</span>',
+						'separator'     => '>',
+						'echo'          => 1
+					);
 
- 		<div class="post_content"><?php the_content(); ?></div>
-		
-		<?php 
+					wp_link_pages( $args );
+					?> 
+					</div> <!-- Post-id content -->	
+					<?php	
 
-		
+				endwhile;//End while
+ 	 	endif;
 
-			$args = array(
-				'before'        => '<div class="clearing"></div><p class"page_links"><span class="links_title"> ' . __( 'Pages:' , 'ony') .'</span>',
-				'after'         => '</p></div>',
-				'link_before'   => '<span>',
-				'link_after'    => '</span>',
-				'separator'     => '>',
-				'echo'          => 1
-			);
-
-			wp_link_pages( $args );		
-
-		endwhile;//End while
- 	else:
- 		echo 'No hemos encontrado posts';
- 	endif;
-		?>
-
-		<?php if (comments_open() || get_comments_number(  )) :?>
+		if (comments_open() || get_comments_number( ) ) :?>
 
 			<div class="comments_section">
 				<div class="section_into">
@@ -59,6 +53,7 @@
 				</div>
 			</div>
 		<?php endif; ?>
+		
  	</main>
  </div>
  <?php get_footer(); ?>
